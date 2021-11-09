2e4570f5956317b691b9d05f1af11110df790c1b
Nils Bollwerk <93703256+Nils4112@users.noreply.github.com>
2021-11-09 15:51:45 +0100

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,43 @@
+public class SelectionSort {
+	
+	
+	static void selectionSort(int array[])
+	{
+		for(int i=0; i<= array.length-1;i++)
+		{
+			int min=array[i];
+			int Imin = 0;
+			for(int t=i;t<array.length;t++)
+			{
+				if(array[t]<min)
+				{
+					min= array[t];
+					Imin= t;
+				}
+			}
+			if(min<array[i])
+			{
+				int temp = array[Imin];
+	            array[Imin] = array[i];
+	            array[i] = temp;
+			}
+			
+		}
+	}
+	static void print(int arr[])
+	{
+		int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+	}
+	public static void main(String args[]) 
+	{
+		int arr[]= {22,34,34,21,46,7,8,9,1,97,99,99};
+		selectionSort(arr);
+		
+		print(arr);
+	}
+	
+	
+}
