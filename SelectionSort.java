db32a3111792d69daebe9e8789a38b2bfa13a44d
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-11-10 17:48:29 +0000

    more linting

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,31 +1,22 @@
 public class SelectionSort {
-	
-	
-	static void sort(int array[])
-	{
-		for(int i = 0; i<= array.length - 1; i++)
-		{
+	static void sort(int array[]) {
+		for(int i = 0; i <= array.length - 1; i++) {
 			int min = array[i];
 			int Imin = 0;
-			for(int t = i; t < array.length; t++)
-			{
-				if(array[t] < min)
-				{
+			for(int t = i; t < array.length; t++) {
+				if(array[t] < min) {
 					min = array[t];
 					Imin = t;
 				}
 			}
-			if(min < array[i])
-			{
+			if(min < array[i]) {
 				int temp = array[Imin];
 	            array[Imin] = array[i];
 	            array[i] = temp;
 			}
-			
 		}
 	}
-	static void print(int arr[])
-	{
+	static void print(int arr[]) {
 		int n = arr.length;
         for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
