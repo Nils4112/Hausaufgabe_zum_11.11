1326dc4758464f28d5ff0315c193d333ae4a332b
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-11-10 17:27:33 +0000

    manual linting

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,19 +3,19 @@ public class SelectionSort {
 	
 	static void selectionSort(int array[])
 	{
-		for(int i=0; i<= array.length-1;i++)
+		for(int i = 0; i<= array.length - 1; i++)
 		{
-			int min=array[i];
+			int min = array[i];
 			int Imin = 0;
-			for(int t=i;t<array.length;t++)
+			for(int t = i; t < array.length; t++)
 			{
-				if(array[t]<min)
+				if(array[t] < min)
 				{
-					min= array[t];
-					Imin= t;
+					min = array[t];
+					Imin = t;
 				}
 			}
-			if(min<array[i])
+			if(min < array[i])
 			{
 				int temp = array[Imin];
 	            array[Imin] = array[i];
@@ -27,13 +27,13 @@ public class SelectionSort {
 	static void print(int arr[])
 	{
 		int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
         System.out.println();
 	}
 	public static void main(String args[]) 
 	{
-		int arr[]= {22,34,34,21,46,7,8,9,1,97,99,99};
+		int arr[]= {22, 34, 34, 21, 46, 7, 8, 9, 1, 97, 99, 99};
 		selectionSort(arr);
 		
 		print(arr);
